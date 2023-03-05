43e049b108f184fff135b7fa1a5c58c4aab47e2d
Volodya Kushch <volodyakushch@gmail.com>
2023-03-05 22:24:11 +0200

    Done

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,5 +1,73 @@
 import React from 'react';
+import Statistics from './Statistics/Statistics';
+import FeedbackOptions from './Feedback/FeedbackOptions ';
+import Section from './Section/Section';
+import Notification from './Notification/Notification';
 
-export const App = () => {
-  return <section>Hello</section>;
-};
+export class App extends React.Component {
+  state = {
+    good: 0,
+    neutral: 0,
+    bad: 0,
+  };
+  getGoodFeetBack = () => {
+    this.setState(prevState => {
+      return {
+        good: prevState.good + 1,
+      };
+    });
+  };
+  getNeutralFeetBack = () => {
+    this.setState(prevState => {
+      return {
+        neutral: prevState.neutral + 1,
+      };
+    });
+  };
+  getBadFeetBack = () => {
+    this.setState(prevState => {
+      return {
+        bad: prevState.bad + 1,
+      };
+    });
+  };
+
+  countTotalFeedback = () => {
+    const { good, bad, neutral } = this.state;
+    let AllFaadback = good + neutral + bad;
+    return AllFaadback;
+  };
+  countPositiveFeedbackPercentage = () => {
+    const { good, bad, neutral } = this.state;
+    let count = 0;
+    count = (good / (good + bad + neutral)) * 100;
+    return Math.round(count) || 0;
+  };
+
+  render() {
+    return (
+      <div>
+        <Section title="Please leave feedback">
+          <FeedbackOptions
+            getGoodFeetBack={this.getGoodFeetBack}
+            getNeutralFeetBack={this.getNeutralFeetBack}
+            getBadFeetBack={this.getBadFeetBack}
+          />
+        </Section>
+        {this.countTotalFeedback() === 0 ? (
+          <Notification />
+        ) : (
+          <Section title="Statistics">
+            <Statistics
+              good={this.state.good}
+              neutral={this.state.neutral}
+              bad={this.state.bad}
+              countTotalFeedback={this.countTotalFeedback()}
+              countPositiveFeedbackPercentage={this.countPositiveFeedbackPercentage()}
+            />
+          </Section>
+        )}
+      </div>
+    );
+  }
+}
--- src/components/App.styled.jsx
+++ src/components/App.styled.jsx
@@ -1,7 +1,1 @@
 import styled from '@emotion/styled';
-
-export const Section = styled.section`
-  margin-left: auto;
-  margin-right: auto;
-  width: 600px;
-`;
